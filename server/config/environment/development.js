867b4f2a88d665fe747aa607524005a1e22d9fc2
Daniel Que <dmdque@gmail.com>
2014-09-13 18:12:22 -0400

    changed db

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,9 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/rpsonline-dev'
+    //uri: 'mongodb://localhost/rpsonline-dev'
+    uri: 'mongodb://daniel:daniel@kahana.mongohq.com:10094/rpsonline'
+
   },
 
   seedDB: true
