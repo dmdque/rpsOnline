bae6baf563cb304fb673de79cf8e37b9a22e4631
Daniel Que <dmdque@gmail.com>
2014-09-14 01:01:20 -0400

    updated landing page

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 rps-myo
 =======
+##Build Info
 Once all necessary dependencies are installed (check generator-angular-fullstack for info), run
 > grunt serve
 
-Or see http://virtualrps.herokuapp.com/arena/lounge
+Or see http://virtualrps.herokuapp.com/arena/lounge for live version.
 
+##Instructions
 Navigate to localhost:9000/arena/room_id with a buddy, and press 'r' for rock, 'p' for paper, or 's' for scissors to play.
 
 Press esc to reset the game in case both players didn't play at the same time.
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,19 @@
 'use strict';
 
 angular.module('rpsOnlineApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
+  .controller('MainCtrl', function ($scope, $http, socket, $location) {
+
+    $scope.play = function() {
+      var text = "";
+      var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+      for( var i=0; i < 5; i++ )
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+      $location.url('arena/' + text);
+    } 
+
+
     $scope.awesomeThings = [];
 
     $http.get('/api/things').success(function(awesomeThings) {
