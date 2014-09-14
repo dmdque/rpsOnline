467097beed74a076f2b6307a9e71a37488a9bba4
Daniel Que <dmdque@gmail.com>
2014-09-14 00:01:31 -0400

    added myo-scripts and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 rps-myo
 =======
+Once all necessary dependencies are installed (check generator-angular-fullstack for info), run
+> grunt serve
+
+Or see http://virtualrps.herokuapp.com/arena/lounge
+
+Navigate to localhost:9000/arena/room_id with a buddy, and press 'r' for rock, 'p' for paper, or 's' for scissors to play.
+
+Press esc to reset the game in case both players didn't play at the same time.
+
+This was built for a Velocity Hackathon, and is meant to be connected to the Thlamic Myo. The Lua script for rock-paper-scissor gesture intergration is very simple and located in the myo-scripts folder.
---
+++ myo-scripts/rock-paper-scissors.lua
@@ -0,0 +1,71 @@
+scriptId = 'com.thalmic.examples.outputeverything'
+ 
+function rock()
+myo.keyboard("r", "press")
+end
+function paper()
+myo.keyboard("p", "press")
+end
+
+function scissors()
+myo.keyboard("s", "press")
+end
+
+function reset()
+myo.keyboard("escape", "press")
+end
+
+-- function up()
+-- myo.keyboard("up_arrow", "press")
+-- end
+
+-- function down()
+--   myo.keyboard("down_arrow", "press")
+-- end
+
+-- function left()
+--   myo.keyboard("left_arrow", "press")
+-- end
+
+-- function right()
+--   myo.keyboard("right_arrow", "press")
+-- end
+ 
+function onPoseEdge(pose, edge)
+  -- myo.debug("edge: " .. edge)
+  pitch = myo.getPitch()
+  myo.debug("pitch: " .. pitch)
+
+  -- myo.debug("onPoseEdge: " .. pose .. ", " .. edge)
+  -- if -0.25 < pitch and pitch < 0.25 and pose == "fist" then
+  --   myo.debug("space")
+  --   myo.keyboard("space", "press")
+  -- end
+  -- if pitch > 0.75 then
+  --   up()
+  -- elseif pitch < 0.25 then
+  --   down()
+  -- end
+  
+  if pitch > 0 and edge == "on" then
+    myo.debug("onPoseEdge: " .. pose .. ", " .. edge)
+    if pose == "fingersSpread" then
+      paper()
+    elseif pose == "fist" then
+      rock()
+    elseif pose == "waveIn" then
+      scissors()
+    elseif pose == "waveOut" then
+      reset()
+    end
+  end
+end
+ 
+function onPeriodic()
+end
+ 
+function onForegroundWindowChange(app, title)
+myo.debug("onForegroundWindowChange: " .. app .. ", " .. title)
+return true
+end
+
